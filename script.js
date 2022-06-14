e9b7c451c137ac209e95a5e194cdee8dd2c25a8c
sahrarafaela <sararafaela704@gmail.com>
2022-06-14 09:18:30 -0300

    primeirosPassosJS

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = 'Sara ';
+let sobrenome= 'Nascimento';
+let anoNascimento = 1999
+let cidade = 'Uberlândia-MG'
+
+console.log(`Eu sou a ${nome + sobrenome}, tenho ${2022-anoNascimento} e moro em ${cidade}`)
